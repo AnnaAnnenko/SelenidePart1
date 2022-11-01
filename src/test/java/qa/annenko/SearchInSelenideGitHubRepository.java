9cc05d235940cd5513f8e5b5b1f91d463098984d
User <annenko.anya@gmail.com>
2022-11-01 12:47:25 +0300

    Add annotation

--- src/test/java/qa/annenko/SearchInSelenideGitHubRepository.java
+++ src/test/java/qa/annenko/SearchInSelenideGitHubRepository.java
@@ -1,16 +1,19 @@
 package qa.annenko;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchInSelenideGitHubRepository {
 
+    @Test
     public void testPathToSoftAssertions() {
             open("https://github.com/selenide/selenide");
             $("#wiki-tab").click();
             $(".wiki-more-pages-link .Link--muted").click();
             $("[data-filterable-for='wiki-pages-filter']").shouldHave(Condition.text("SoftAssertions"));
             $("a[href$='SoftAssertions']").click();
+            $("#wiki-body").shouldHave(Condition.text("Using JUnit5"));
     }
 }
