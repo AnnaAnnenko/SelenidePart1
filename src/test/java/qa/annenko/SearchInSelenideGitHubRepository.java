94d5a3dbb17224c006807a4346528a0d8cf6d3c6
User <annenko.anya@gmail.com>
2022-11-01 12:32:55 +0300

    Add new test

--- src/test/java/qa/annenko/SearchInSelenideGitHubRepository.java
+++ src/test/java/qa/annenko/SearchInSelenideGitHubRepository.java
@@ -2,14 +2,15 @@ package qa.annenko;
 
 import com.codeborne.selenide.Condition;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchInSelenideGitHubRepository {
 
-    public void testSearchInGitHub() {
-        open("https://github.com/selenide/selenide");
-        $(".wiki-tab").click();
-        $(".filter-bar").shouldHave(Condition.text("SoftAssertions"));
+    public void testPathToSoftAssertions() {
+            open("https://github.com/selenide/selenide");
+            $("#wiki-tab").click();
+            $(".wiki-more-pages-link .Link--muted").click();
+            $("[data-filterable-for='wiki-pages-filter']").shouldHave(Condition.text("SoftAssertions"));
+            $("a[href$='SoftAssertions']").click();
     }
 }
